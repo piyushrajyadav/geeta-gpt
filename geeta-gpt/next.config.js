87b1029171fa8b4db5b6b40322f8b447e1b7dc2a
piyushrajyadav <piyushyadavrajyadav@gmail.com>
2025-03-11 10:22:40 +0530

    save changes

--- geeta-gpt/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  optimizeFonts: true,
-  images: {
-    domains: ['fonts.googleapis.com', 'fonts.gstatic.com'],
-  },
-}
-
-module.exports = nextConfig 
